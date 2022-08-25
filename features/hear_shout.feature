74295ed84db1ce0ff4ecfcda4c532cde711e6d68
Jack Guthrie <jackg3323@gmail.com>
2022-08-25 18:51:42 +0100

    Implement Cucumber test and refine Gherkin scenario

--- features/hear_shout.feature
+++ features/hear_shout.feature
@@ -5,6 +5,9 @@ Feature: Hear shout
    Rule: Shouts can be heard by everyone within range.
 
       Scenario: Listener is within range
-         Given Lucy is located 15 meters away from Sean
+         # Given Lucy is located 15 meters away from Sean
+         Given the range is 100
+         And Sean is located at 0
+         And Lucy is located at 50
          When Sean shouts "Hello"
          Then Lucy hears Sean's Shout
--- features/step_definitions/steps.mjs
+++ features/step_definitions/steps.mjs
@@ -1,17 +1,41 @@
 import { Given, Then, When } from "@cucumber/cucumber";
 import { expect } from "expect";
+import Network from "../../src/models/Network.js";
+import Person from "../../src/models/Person.js";
 
-Given("Lucy is located {float} meters away from Sean", function (distance) {
-  this.distance = distance;
-  this.listener = "Lucy";
-  this.shouter = "Sean";
+// Given("Lucy is located {float} meters away from Sean", function (distance) {
+//   this.network = new Network();
+//   this.listener = new Person({
+//     name: "Lucy",
+//     network: this.network,
+//     position: distance,
+//   });
+//   this.shouter = new Person({ name: "Sean", network: this.network });
+// });
+
+Given("the range is {float}", function (range) {
+  // Write code here that turns the phrase above into concrete actions
+  this.network = new Network(range);
+});
+
+Given("Sean is located at {float}", function (shouterPos) {
+  // Write code here that turns the phrase above into concrete actions
+  this.shouter = new Person({ name: "Sean", network: this.network });
+});
+
+Given("Lucy is located at {float}", function (listenerPos) {
+  // Write code here that turns the phrase above into concrete actions
+  this.listener = new Person({
+    name: "Lucy",
+    network: this.network,
+    position: listenerPos,
+  });
 });
 
 When("Sean shouts {string}", function (msg) {
   this.shouter.shout(msg);
-  this.msg = msg;
 });
 
 Then("Lucy hears Sean's Shout", function () {
-  expect(this.listener.msg).toContain(this.msg);
+  expect(this.listener.messages).toEqual(this.shouter.shouts);
 });
